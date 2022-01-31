7d67ef039b887faaebc6ba97f78dd0457cea9b99
kenshin1027 <cd1029@163.com>
2022-01-31 23:53:46 +0800

    111

--- game_functions.py
+++ game_functions.py
@@ -97,6 +97,20 @@ def create_alien(ai_settings,screen,aliens,alien_number,row_number):
 	alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
 	aliens.add(alien)
 
-def update_aliens(aliens):
+def update_aliens(ai_settings,aliens):
 	"""更新外星人群中所有外星人的位置"""
+	check_fleet_edges(ai_settings,aliens)
 	aliens.update()
+
+def check_fleet_edges(ai_settings,aliens):
+	"""有外星人到达边缘时采取相应措施"""
+	for alien in aliens.sprites():
+		if alien.check_edges():
+			change_fleet_direction(ai_settings,aliens)
+			break
+
+def change_fleet_direction(ai_settings,aliens):
+	"""将整群外星人下移，并改变它们的方向"""
+	for alien in aliens.sprites():
+		alien.rect.y += ai_settings.fleet_drop_speed
+	ai_settings.fleet_direction *= -1
