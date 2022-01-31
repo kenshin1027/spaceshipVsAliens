31369d24164edc8ab4c9f055a672b8bc8d5a22d0
kenshin1027 <cd1029@163.com>
2022-02-01 00:16:25 +0800

    44

--- alien_invasion.py
+++ alien_invasion.py
@@ -20,13 +20,13 @@ def run_game():
 	bullets = Group()
 	# 创建一个外星人的编组
 	aliens = Group()
-	gf.create_fleet(ai_settings,screen,ship,aliens)
+	gf.create_fleet(ai_settings,screen,ship,aliens)	
 
 	# 开始游戏的主循环
 	while True:
 		gf.check_events(ai_settings,screen,ship,bullets)
 		ship.update()
-		gf.update_bullets(aliens,bullets)
+		gf.update_bullets(ai_settings,screen,ship,aliens,bullets)
 		gf.update_aliens(ai_settings,aliens)
 		gf.update_screen(ai_settings,screen,ship,aliens,bullets)
 
--- game_functions.py
+++ game_functions.py
@@ -49,7 +49,7 @@ def update_screen(ai_settings,screen,ship,aliens,bullets):
 	# 让最近绘制的图像可见
 	pygame.display.flip()
 
-def update_bullets(aliens,bullets):
+def update_bullets(ai_settings,screen,ship,aliens,bullets):
 	"""更新子弹的位置，并删除已经消失的子弹"""
 	bullets.update()
 
@@ -61,6 +61,11 @@ def update_bullets(aliens,bullets):
 	# 检查是否有子弹击中了外星人
 	# 如果是这样，就删除相应的子弹和外星人
 	collisions = pygame.sprite.groupcollide(bullets,aliens,True,True)
+
+	if  len(aliens) == 0:
+		# 删除现有的子弹并新建一群外星人
+		bullets.empty()
+		create_fleet(ai_settings,screen,ship,aliens)
 
 def fire_bullet(ai_settings,screen,ship,bullets):
 	"""如果还没有达到限制，就发射一颗子弹"""
