dc9ed96a22194bfc6fc8b897dc87f526ba245c30
kenshin1027 <cd1029@163.com>
2022-02-01 16:47:38 +0800

    55

--- settings.py
+++ settings.py
@@ -12,8 +12,8 @@ class Settings():
 		self.ship_speed_factor = 1.5
 
 		# 子弹设置
-		self.bullet_speed_factor = 1
-		self.bullet_width = 200
+		self.bullet_speed_factor = 2
+		self.bullet_width = 5
 		self.bullet_height = 15
 		self.bullet_color = 60,60,60
 		self.bullets_allowed = 20
